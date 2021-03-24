fe2b1d4b14f2ed354574312ae11d4e9ff1381389
Andrew Robertson <2850052+adrdc@users.noreply.github.com>
2021-03-24 14:07:43 -0400

    Issue 8: Re-enable json row decoder (#17)
    Signed-off-by: Andrew Robertson <andrew.robertson@dell.com>

--- src/main/java/io/pravega/connectors/presto/decoder/JsonRowDecoder.java
+++ src/main/java/io/pravega/connectors/presto/decoder/JsonRowDecoder.java
@@ -12,27 +12,76 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Note: This file contains changes from PrestoDb.  Specifically the decodeTree and locateNode methods
+ * (rev cb21100597c1aec4c6f29e4a82117886aaa95d10)
+ * https://github.com/prestodb/presto/blob/0.235/presto-record-decoder/src/main/java/com/facebook/presto/decoder/json/JsonRowDecoder.java
  */
 
 package io.pravega.connectors.presto.decoder;
 
+import com.facebook.presto.decoder.DecoderColumnHandle;
+import com.facebook.presto.decoder.FieldValueProvider;
+import com.facebook.presto.decoder.json.JsonFieldDecoder;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.MissingNode;
+import com.google.common.base.Splitter;
 import io.pravega.connectors.presto.PravegaRecordValue;
-// import io.pravega.connectors.presto.TypedRecordValue;
+import io.pravega.connectors.presto.TypedRecordValue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkState;
 
 public class JsonRowDecoder
         implements EventDecoder
 {
-    private final com.facebook.presto.decoder.json.JsonRowDecoder jsonRowDecoder;
+    private final ObjectMapper objectMapper;
 
-    public JsonRowDecoder(com.facebook.presto.decoder.json.JsonRowDecoder jsonRowDecoder)
+    private final Map<DecoderColumnHandle, JsonFieldDecoder> fieldDecoders;
+
+    public JsonRowDecoder(ObjectMapper objectMapper, Map<DecoderColumnHandle, JsonFieldDecoder> fieldDecoders)
     {
-        this.jsonRowDecoder = jsonRowDecoder;
+        this.objectMapper = objectMapper;
+        this.fieldDecoders = fieldDecoders;
     }
 
     @Override
     public boolean decodeEvent(DecodableEvent event, PravegaRecordValue record)
     {
-        // ((TypedRecordValue) record).setDecodedValue(jsonRowDecoder.decodeTree(event.asJson()));
+        ((TypedRecordValue) record).setDecodedValue(decodeTree(event.asJson()));
         return true;
+    }
+
+    public Optional<Map<DecoderColumnHandle, FieldValueProvider>> decodeTree(JsonNode tree)
+    {
+        Map<DecoderColumnHandle, FieldValueProvider> decodedRow = new HashMap<>();
+
+        for (Map.Entry<DecoderColumnHandle, JsonFieldDecoder> entry : fieldDecoders.entrySet()) {
+            DecoderColumnHandle columnHandle = entry.getKey();
+            JsonFieldDecoder decoder = entry.getValue();
+            JsonNode node = locateNode(tree, columnHandle);
+            decodedRow.put(columnHandle, decoder.decode(node));
+        }
+
+        return Optional.of(decodedRow);
+    }
+
+    private static JsonNode locateNode(JsonNode tree, DecoderColumnHandle columnHandle)
+    {
+        String mapping = columnHandle.getMapping();
+        checkState(mapping != null, "No mapping for %s", columnHandle.getName());
+
+        JsonNode currentNode = tree;
+        for (String pathElement : Splitter.on('/').omitEmptyStrings().split(mapping)) {
+            if (!currentNode.has(pathElement)) {
+                return MissingNode.getInstance();
+            }
+            currentNode = currentNode.path(pathElement);
+        }
+        return currentNode;
     }
 }
